7be3138f2cff878eda3822880186c937a4055c1b
Coding Marquice <49505193+GoldenMarquice@users.noreply.github.com>
2023-05-14 11:28:43 -0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,70 @@
+console.log("Linked")
+console.log("This is a message");
+    console.warn("This is a warning");
+    console.error("This is an error");
+   
+   
+    var userName = "Adam";
+    var student1 = "Adam"; //declaration and assign
+    var student2 = "Marquice";
+    var student3 = "DeJuan";
+    var num1 = 10;
+    var num2 = 5;
+
+    //netflix
+
+    var movie = "James Bond";
+    var movie1 = "Air Jordan";
+    var movie2 = "Kevin Hart";
+    var movie3 = "Dave";
+    var movie4 = "Oceans";
+
+    //Amazon
+
+    var item = "cup";
+    var item1 = "shoes";
+    var item2 = "pants";
+    var item3 = "socks";
+    var item4 = "airfryer";
+
+    //Youtube
+
+    var channel = "lofi";
+    var channel1 = "Mr.Beast";
+    var channel2 = "Cars";
+    var channel3 = "Shoes";
+    var channel4 = "football";
+    var channel5 = "Basketball";
+    var channel6 = "highlights";
+    var channel7 = "future";
+    var channel8 = "cooking";
+    var channel9 = "Shopping";
+
+    document.write(` <p> ${movie} </p>`);
+    document.write(` <p> ${movie1} </p>`);
+    document.write(` <p> ${movie2} </p>`);
+    document.write(` <p> ${movie3} </p>`);
+    document.write(` <p> ${movie4} </p>`);
+    document.write(` <p> ${item} </p>`);
+    document.write(` <p> ${item1} </p>`);
+    document.write(` <p> ${item2} </p>`);
+    document.write(` <p> ${item3} </p>`);
+    document.write(` <p> ${item4} </p>`);
+
+    document.write(` <p> ${channel} </p>`);
+    document.write(` <p> ${channel1} </p>`);
+    document.write(` <p> ${channel2} </p>`);
+    document.write(` <p> ${channel3} </p>`);
+    document.write(` <p> ${channel4} </p>`);
+    document.write(` <p> ${channel5} </p>`);
+    document.write(` <p> ${channel6} </p>`);
+    document.write(` <p> ${channel7} </p>`);
+    document.write(` <p> ${channel8} </p>`);
+    document.write(` <p> ${channel9} </p>`);
+  
+
+    document.write(` <p> ${student1} </p>`);
+    document.write(`<p> ${student2} </p>`);
+    document.write(`<p> ${student3} </p>`);
+    var sum = num1 + num2;
+    document.write(`<p> ${sum} </p>`);
